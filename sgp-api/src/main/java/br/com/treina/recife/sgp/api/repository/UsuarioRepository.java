70d7fc7fa9b1ab9ef8735ac220ee314573b736de
Alexandre Junior <souzajr.alexandre@gmail.com>
2026-03-09 21:45:01 -0300

    Consultas personalizada de Usuario

--- sgp-api/src/main/java/br/com/treina/recife/sgp/api/repository/UsuarioRepository.java
+++ sgp-api/src/main/java/br/com/treina/recife/sgp/api/repository/UsuarioRepository.java
@@ -1,11 +1,29 @@
 package br.com.treina.recife.sgp.api.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.treina.recife.sgp.api.model.Usuario;
+import java.time.LocalDate;
+
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
+
+    Optional<Usuario> findByEmail(String email);
+
+    Optional<Usuario> findByCpf(String cpf);
+
+    Optional<Usuario> findByEmailAndSenha(String email, String senha);
+
+    List<Usuario> findByStatus(String status);
+
+    List<Usuario> findByDataNascimentoBetween(LocalDate dataInicio, LocalDate dataFim);
+
+    // *andre -> Alexandre, Andreza, Andrea, Andressa, Andrelina, Andrezza
+    List<Usuario> findByNomeContainingIgnoreCase(String seqCaracteres);
+
 }
